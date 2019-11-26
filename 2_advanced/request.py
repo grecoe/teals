e75bd5f4760c5da7f790339194e6552715c0420b
grecoe <grecoe@microsoft.com>
2019-11-25 19:37:45 -0500

    Using public endpoint for requests.py

--- 2_advanced/request.py
+++ 2_advanced/request.py
@@ -2,70 +2,46 @@
     Making HTTP requests to any number of services has become even more important with all of the networking and 
     cloud solutions out there. 
 
-    Here ia simple example on how to use the requests library to make calls to a RESTful Web Service. The service was created in AWS
-    and is also Python based:
+    Here ia simple example on how to use the requests library to make calls to a RESTful Web Service. 
+    
+    The service provides news stories and you can access the information with these links.
+    
+    To show you how this works, this will get the latest stories and print out
+    1. What organziation published it
+    2. The author
+    3. The title
 
-    Function Code:
-
-import json
-
-def lambda_handler(event, context):
-
-    returnData = {}
-
-
-    # Depending on how we are called, we do different things. 
-    try:     
-        if event['httpMethod'] == 'POST':
-            # Load the resquest body     
-            bodyobj = json.loads(event["body"])
-            responseBodyValue = bodyobj['name'] +  " wishes you well on " + bodyobj["project"]
-            returnData["trigger"] = "POST"
-            returnData["post_message"] = responseBodyValue
-        elif event['httpMethod'] == 'GET':
-            returnData["trigger"] = "GET"
-            returnData["get_message"] = "A GET occured, should give you something? :)"
-        else:
-            returnData["trigger"] = event['httpMethod']
-    except Exception as ex:
-        returnData["exception"] = str(ex)
-
-
-    return {
-        'statusCode': 200,
-        'body' : json.dumps(returnData)
-    }
+    This data is provided by : https://newsapi.org/
+    Documentation: https://newsapi.org/docs/get-started
 
 '''
 import requests 
 import json
 
-service_url = 'https://ebqjqw5214.execute-api.us-east-2.amazonaws.com/firstStage/test'
-header = {}
-header['x-api-key'] = "dJLUvZW8lN3M6xZFWeqcq7I25dxRam785OFvo5mn"
+# This is the country code, try de for German!
+country_code = "us"
+news_api_key = "4804eefdde4645309b2687aa495b590e"
+service_url = "https://newsapi.org/v2/top-headlines?country={}&apiKey={}".format(country_code, news_api_key)
 
-
-post_request = False
-request_result = None
-
-if post_request:
-    # Make a POST request
-    request_body = {'name': 'Mr. Norton', "project" : "Class Assignment"}
-    formatted_body = json.dumps(request_body)
-    request_result = requests.post(url = service_url, data = formatted_body, headers = header)
-else:
-    # Make a GET request
-    request_result = requests.get(url = service_url, headers = header)
+# Make a GET request
+request_result = requests.get(url = service_url)
 
 if request_result :
     # We have some result, so lets print a little bit out. Remember, we know what the 
     # backend code looks like so we know what to look for in the body. 
     print("CALL STATUS -", request_result.status_code)
-    result_body = json.loads(request_result.text)
-    if post_request :
-        print("MESSAGE FOR YOU: " , result_body['post_message'])
-    else:
-        print("MESSAGE FOR YOU: " , result_body['get_message'])
+
+    if(request_result.status_code == 200):
+        result_body = json.loads(request_result.text)
+
+        print("Status : ", result_body["status"])
+        print("Article Count : ", result_body["totalResults"])
+        for article in result_body["articles"]:
+            print("Source : ", article["source"]["name"])
+            print("Author : ", article["author"])
+            print("Title  : ", article["title"])
+            print("")
+
 
 
 
