c7d9c9cde08a1633f966ee4a880c522991fda707
grecoe <grecoe@microsoft.com>
2020-12-16 14:14:42 -0500

    Add war to menu

--- 8_game_controller/utils/logger.py
+++ 8_game_controller/utils/logger.py
@@ -11,11 +11,15 @@ class Logger:
         with append only. Careful this could create an enormous log.
         """
         output = "\n{}\t{}".format(str(datetime.now()), message)
-        with open(controller_log, 'a') as log_file:
-            log_file.writelines(output)
+        with open(controller_log, 'a', encoding="utf-8") as log_file:
+            try:
+                log_file.writelines(output)
+            except Exception as ex:
+                output = "\n{}\t{}".format(str(datetime.now()), "LOG ERROR: {}".format(str(ex)))
+                log_file.write(output)
 
     @staticmethod
-    def clear_log(max_size=102400, controller_log='controller.log'):
+    def clear_log(max_size=2097152, controller_log='controller.log'):
         """
         Checks the size of the log and renames it if it is already
         a certain size.
@@ -25,4 +29,4 @@ class Logger:
             print("Log is {} bytes".format(size))
             if size >= max_size:
                 os.remove(controller_log)
-                Logger.add_log("Log file cleared on size...")
+                Logger.add_log("Log file cleared on size... {}".format(size))
