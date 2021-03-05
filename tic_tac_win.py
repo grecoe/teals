b69b9b4c18c52b29afb379c06cd21e53817d0f0b
grecoe <grecoe@microsoft.com>
2021-03-05 12:50:03 -0500

    Fuller TTT solvers

--- tic_tac_win.py
+++ tic_tac_win.py
@@ -1,3 +1,132 @@
+"""
+This file contins several options for solving the winner in tic tac toe problem.
+
+If your program uses a single list to represent the board OR a list of lists there is a solution
+
+    single list:    checkWinner
+                    advanced_checkWinner
+
+    list of list:   checkWinnerArr
+                    advanced_checkWinnerArr
+
+At the bottom of the file there are two functions to generate random boards with a column, row
+or diagonal prefilled to test out the solvers.
+
+Once comfortable with it, bring it into your program!
+"""
+import math
+import random
+
+
+def check_collection(collection, size, default_value):
+    """
+    No matter what check you are doing - straight list board, list of list board you have to check
+    collections for a winner. Break that out to do the validation and it can be used in both.
+
+    Params:
+        collection - List to check
+        size - Expected size of the collection
+        default_value - Value to ignore in collection check.
+
+    Returns:
+        True if the collection has a set size of 1 and is NOT default value.
+    """
+    if len(collection) == size and len(set(collection)) == 1 and collection[0] != default_value:
+        return True
+    return False
+
+
+def advanced_checkWinner(board, default_value=''):
+
+    width = (int)(math.sqrt(len(board)))
+
+    if((width * width) != len(board)):
+        raise Exception("Invalid board")
+
+    winner = None
+
+    # Have to check rows and columns but collect the diagonals as well
+    forward_diag = []
+    backward_diag = []
+    for row in range(width):
+        # Collect diagonals
+        forward_diag.append(board[row * width + row])
+        backward_diag.append(board[(row * width) + (width - row - 1)])
+
+        # A place to collect rows and columns
+        curRow = []
+        curCol = []
+
+        # Get next row/column
+        for col in range(width):
+            curRow.append(board[(row * width) + col])
+            curCol.append(board[(col * width) + row])
+
+        # Check for a winner in this row/column pairing
+        if check_collection(curRow, width, default_value):
+            winner = curRow[0]
+        if check_collection(curCol, width, default_value):
+            winner = curCol[0]
+
+        if winner:
+            # If we have a winner just get out so we dont keep iterating the board
+            break
+
+    if not winner:
+        if check_collection(forward_diag, width, default_value):
+            winner = forward_diag[0]
+        if check_collection(backward_diag, width, default_value):
+            winner = backward_diag[0]
+
+    return winner
+
+
+def advanced_checkWinnerArr(board, default_value=' '):
+
+    # Verify board
+    for row in board:
+        if len(row) != len(board):
+            raise Exception("Invalid board")
+
+    width = len(board)
+    winner = None
+
+    # Each iteration here checks a row (easy) and a column (harder)
+    for row in range(width):
+        if check_collection(board[row], width, default_value):
+            # Check each row as you go, if we have a hit it will save us time.
+            winner = board[row][0]
+        else:
+            # Get next column row now becomes the column idx
+            col_data = []
+            for act_row in range(width):
+                col_data.append(board[act_row][row])
+
+            if check_collection(col_data, width, default_value):
+                winner = col_data[0]
+
+        if winner:
+            break
+
+    # Check forward diagonal (easy)
+    if not winner:
+        forward_diag = []
+        backward_diag = []
+
+        # Forward is easy, backwards slightly harder
+        back_row_idx = width - 1
+        for base_idx in range(len(board[0])):
+            forward_diag.append(board[base_idx][base_idx])
+            backward_diag.append(board[back_row_idx][base_idx])
+            back_row_idx -= 1
+
+        if check_collection(forward_diag, width, default_value):
+            winner = forward_diag[0]
+        if check_collection(backward_diag, width, default_value):
+            winner = backward_diag[0]
+
+    return winner
+
 
 def checkWinnerArr(board, default_value=' '):
     """
@@ -120,26 +249,69 @@ def checkWinner(board, width, default_value=' '):
     return winner
 
 
-# Generate random sized list based on width
-loc = 0
-width = 20
-large_list = [x for x in (' ' * (width * width))]
-while loc < len(large_list):
-    large_list[loc] = 'Z'
-    loc += width
+def build_single_list(width, default_val, set_random_row=False, set_random_col=False, set_diag=False):
+    loc = 0
+    single_list = [x for x in (default_val * (width * width))]
 
-#print(large_list)
-#print("Basic list representing board of {}x{} :  Z  winner - {}".format(width, width, checkWinner(large_list, width)))
+    rand_selection = random.randint(0, width-1)
+    if set_random_row:
+        loc = (rand_selection * width)
+        for idx in range(loc, (loc + width)):
+            single_list[idx] = 'R'
+    elif set_random_col:
+        loc = rand_selection
+        while loc < len(single_list):
+            single_list[loc] = 'C'
+            loc += width
+    elif set_diag:
+        loc = 0
+        while loc < len(single_list):
+            single_list[loc] = 'D'
+            loc += width + 1
+
+    print(single_list)
+    return single_list
 
 
+def build_list_of_list(width, default_val, set_random_row=False, set_random_col=False, set_diag=False):
+    loc = 0
+    board = []
+
+    rand_selection = random.randint(0, width-1)
+    if set_random_row:
+        for i in range(width):
+            chartouse = default_val if i != rand_selection else 'R'
+            board.append([x for x in (chartouse * width)])
+    elif set_random_col:
+        for i in range(width):
+            board.append([x for x in (default_val * width)])
+            board[-1][rand_selection] = 'C'
+    elif set_diag:
+        for i in range(width):
+            board.append([x for x in (default_val * width)])
+            board[-1][i] = 'D'
+
+    for row in board:
+        print(row)
+    return board
 
 
-# Now make a XxX board but set diagonal to R based on width
-print("List of lists")
-board = []
-for i in range(width):
-    chartouse = ' ' if i != 4 else 'A'
-    board.append([x for x in (chartouse * width)])
-    board[-1][i] = 'R' if chartouse != 'A' else chartouse
-    print(board[-1])
-print("List of Lists - {}x{} A is Winner - {}".format(width, width, checkWinnerArr(board)))
+# Test it all out
+total_width = 5
+
+print("Test single list - width", total_width)
+lst = build_single_list(total_width, ' ', True, False, False)
+print("List row winner : ", advanced_checkWinner(lst, ' '))
+lst = build_single_list(total_width, ' ', False, True, False)
+print("List column winner : ", advanced_checkWinner(lst, ' '))
+lst = build_single_list(total_width, ' ', False, False, True)
+print("List diagonal winner : ", advanced_checkWinner(lst, ' '))
+
+print("\nTest list of lists - width", total_width)
+bd = build_list_of_list(total_width, ' ', True, False, False)
+print("List row winner : ", advanced_checkWinnerArr(bd, ' '))
+bd = build_list_of_list(total_width, ' ', False, True, False)
+print("List column winner : ", advanced_checkWinnerArr(bd, ' '))
+bd = build_list_of_list(total_width, ' ', False, False, True)
+print("List diagonal winner : ", advanced_checkWinnerArr(bd, ' '))
+
