b5a9006514ac3541486dbaf563cadabafcb6619b
grecoe <grecoe@microsoft.com>
2021-03-05 08:18:13 -0500

    List

--- tic_tac_win.py
+++ tic_tac_win.py
@@ -1,5 +1,5 @@
 
-def checkWinnerArr(board):
+def checkWinnerArr(board, default_value=' '):
     """
     Function to check winner of tic-tac-toe IF your board is an list
     of lists, i.e. [] [],[],...]
@@ -19,7 +19,7 @@ def checkWinnerArr(board):
 
     # Check rows - for every row check to see if they are the same
     for i in range(len(board[0])):
-        if len(set(board[i])) == 1:
+        if len(set(board[i])) == 1 and board[i][0] != default_value:
             winner = board[i][0]
 
     # Check columns
@@ -29,7 +29,7 @@ def checkWinnerArr(board):
             for row in range(len(board)):
                 col_data.append(board[row][col])
 
-            if len(set(col_data)) == 1:
+            if len(set(col_data)) == 1 and col_data[0] != default_value:
                 winner = col_data[0]
 
     # Check forward diagonal (easy)
@@ -38,7 +38,7 @@ def checkWinnerArr(board):
         for col_row in range(len(board[0])):
             diag.append(board[col_row][col_row])
 
-        if len(set(diag)) == 1:
+        if len(set(diag)) == 1 and diag[0] != default_value:
             winner = diag[0]
 
     # Check backwards diag
@@ -51,13 +51,13 @@ def checkWinnerArr(board):
             start_row -= 1
             start_col += 1
 
-        if len(set(diag)) == 1:
+        if len(set(diag)) == 1 and diag[0] != default_value:
             winner = diag[0]
 
     return winner
 
 
-def checkWinner(board, width):
+def checkWinner(board, width, default_value=' '):
     """
     Function to check winner of tic-tac-toe IF your board is a single array
     that represents all the slots, i.e. [....]
@@ -89,9 +89,9 @@ def checkWinner(board, width):
         # Set function returns a collection of unique items
         # in an enumerable type (list in this case). If the count
         # is 1 then we have a winner. Save the winners mark.
-        if len(set(curRow)) == 1:
+        if len(set(curRow)) == 1 and curRow[0] != default_value:
             winner = curRow[0]
-        if len(set(curCol)) == 1:
+        if len(set(curCol)) == 1 and curCol[0] != default_value:
             winner = curCol[0]
 
         if winner:
@@ -111,29 +111,35 @@ def checkWinner(board, width):
             backward.append(board[(row * width) + (width - row - 1)])
 
         # Again use set() to figure out if we have a winner.
-        if len(set(forward)) == 1:
+        if len(set(forward)) == 1 and forward[0] != default_value:
             winner = forward[0]
-        if len(set(backward)) == 1:
+        if len(set(backward)) == 1 and backward[0] != default_value:
             winner = backward[0]
 
     # returns the winners mark, if this is empty no winner.
     return winner
 
 
-# Base board
-board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-print("Standard List - 3x3 No Winner - ", checkWinner(board, 3))
+# Generate random sized list based on width
+loc = 0
+width = 20
+large_list = [x for x in (' ' * (width * width))]
+while loc < len(large_list):
+    large_list[loc] = 'Z'
+    loc += width
 
-# Winning board - first column
-board = ["A", "2", "3", "4", "A", "6", "7", "8", "A", "10", "11","12", "A", "14", "15","16"]
-print("Standard List - 4x4 A is Winner - ", checkWinner(board, 4))
+#print(large_list)
+#print("Basic list representing board of {}x{} :  Z  winner - {}".format(width, width, checkWinner(large_list, width)))
 
 
-# Again but with lists
-board = [
-    ["1", "2", "3","A"],
-    ["5", "6", "A", "8"],
-    ["9", "A", "11", "12"],
-    ["A", "14", "15", "16"]
-]
-print("List of Lists - 4x4 A is Winner - ", checkWinnerArr(board))
+
+
+# Now make a XxX board but set diagonal to R based on width
+print("List of lists")
+board = []
+for i in range(width):
+    chartouse = ' ' if i != 4 else 'A'
+    board.append([x for x in (chartouse * width)])
+    board[-1][i] = 'R' if chartouse != 'A' else chartouse
+    print(board[-1])
+print("List of Lists - {}x{} A is Winner - {}".format(width, width, checkWinnerArr(board)))
